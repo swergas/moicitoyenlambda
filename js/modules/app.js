787a7867ded661a7293b8893c9fbac05549590a8
swergas <quentin.grimaud@gmail.com>
2022-02-11 22:08:43 +0100

    ajoute footer

--- js/modules/app.js
+++ js/modules/app.js
@@ -7,10 +7,104 @@ const e = React.createElement;
 const Button = ReactBootstrap.Button;
 
 const Footer = () => {
+  /*
   return e(
     Button,
     null,
     'test'
+  );
+  */
+  return e(
+    "div",
+    {
+      className: "footer mt-5"
+    },
+    e(
+      "div",
+      {
+        className: "text-center"
+      },
+      e(
+        "h3",
+        null,
+        "Rejoignez-nous !"
+      ),
+      e(
+        "a",
+        {
+          href: "https://www.facebook.com/MoiCitoyenLambda"
+        },
+        e(
+          "img",
+          {
+            src: "img/logo_facebook.png",
+            width: "100px",
+            alt: "Logo Facebook"
+          }
+        )
+      ),
+      e(
+        "a",
+        {
+          href: "https://www.youtube.com/channel/UCQvc3gFmN9PKcogaCv3sobw"
+        },
+        e(
+          "img",
+          {
+            src: "img/logo_youtube.png",
+            width: "100px",
+            alt: "Logo Youtube"
+          }
+        )
+      ),
+      e(
+        "a",
+        {
+          href: "mailto:contact@moicitoyenlambda.fr"
+        },
+        e(
+          "img",
+          {
+            src: "img/logo_message.png",
+            width: "100px",
+            alt: "Logo Message"
+          }
+        )
+      ),
+      e(
+        "a",
+        {
+          href: "https://discord.gg/vpZzdzZ"
+        },
+        e(
+          "img",
+          {
+            src: "img/logo_discord.png",
+            width: "100px",
+            alt: "Logo Discord"
+          }
+        )
+      ),
+      e(
+        "a",
+        {
+          href: "https://twitter.com/moi_citoyen"
+        },
+        e(
+          "img",
+          {
+            src: "img/logo_twitter.png",
+            width: "100px",
+            alt: "Logo Twitter"
+          }
+        )
+      ),
+      e(
+        "p",
+        null,
+        "contact@moicitoyenlambda.fr"
+      )
+    )
   );
 };
 
